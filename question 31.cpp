d51a2b250668b504571c4251e9cd25e99ba1ebe4
Milton Chandro Bhowmick <miltonbhowmick6@gmail.com>
2020-02-04 12:19:07 +0600

    Add files via upload

--- question 31.cpp
+++ question 31.cpp
@@ -1,6 +1,25 @@
 
 /// You have to find the node which has highest possibility to reach after t time
 /// from the source node!!!
+/*
+SAMPLE
+1
+6 12 30
+0 1 0.3
+0 2 0.2
+0 5 0.5
+1 3 0.6
+1 5 0.4
+2 0 0.3
+2 4 0.7
+3 0 1
+4 3 1
+5 0 0.4
+5 3 0.2
+5 4 0.4
+0
+*/
+
 
 #include<iostream>
 #define MAX 1000
