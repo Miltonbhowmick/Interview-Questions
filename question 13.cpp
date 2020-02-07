2f6485db5340f71f8167e70e9b37be0ae7655751
Milton Chandro Bhowmick <miltonbhowmick6@gmail.com>
2020-02-07 23:12:18 +0600

    Add files via upload

--- question 13.cpp
+++ question 13.cpp
@@ -1,57 +1,53 @@
+/*
+3
+0 0 50 50
+1 2 4 5 10
+3 4 6 7 20
+10 20 15 18 30
+
+
+*/
 #include<iostream>
+#define MAX 1000
 using namespace std;
-int graph[10000][10000], point[1000][5];
-bool visited[10000];
+int grid_worm[MAX][5],n,sx,sy,dx,dy,ans,dis;
+bool vis[MAX];
+/// bidirectional wormholes!!
 
-int abs(int a){
-    if(a<0) return -1*a;
-    return a;
+int abs(int x){
+    if(x<0) return-1*x;
+    return x;
 }
-
-int distance(int sx,int sy,int dx, int dy){
+int distance(int sx,int sy,int dx,int dy){
     return abs(sx-dx)+abs(sy-dy);
 }
-void short_dis_to(int n,int sx,int sy,int dx,int dy,int cost,int &ans){
+void fn(int sx,int sy,int dx,int dy,int dis){
+    ans=min(ans, distance(sx,sy,dx,dy)+dis);
 
-    ans=min(ans,distance(sx,sy,dx,dy)+cost);
     for(int i=0;i<n;i++){
-        if(visited[i]==false){
-            visited[i]=true;
-            int temp=distance(sx,sy,point[i][0],point[i][1])+point[i][4]+cost;
-            short_dis_to(n,point[i][2],point[i][3],dx,dy,temp,ans);
-            visited[i]=false;
-        }
-    }
-}
-
-void short_dis_by(int n, int sx,int sy,int dx,int dy,int cost,int &ans){
-    ans=min(ans, distance(sx,sy,dx,dy)+cost);
-    for(int i=n-1;i>=0;i--){
-        if(visited[i]==false){
-            visited[i]=true;
-            int temp=distance(sx,sy,point[i][2],point[i][3])+point[i][4]+cost;
-            short_dis_by(n,point[i][0],point[i][1],dx,dy,temp,ans);
-            visited[i]=false;
+        if(vis[i]==false){
+            vis[i]=true;
+            /// Bidirectional IMPORTANT!!
+            int temp=distance(sx,sy,grid_worm[i][0],grid_worm[i][1])+grid_worm[i][4]+dis;
+            fn(grid_worm[i][2],grid_worm[i][3],dx,dy,temp);
+            temp=distance(sx,sy,grid_worm[i][2],grid_worm[i][3])+grid_worm[i][4]+dis;
+            fn(grid_worm[i][0],grid_worm[i][1],dx,dy,temp);
+            vis[i]=false;
         }
     }
 }
 int main()
 {
-    int t,n;
-    cin>>t;
-    while(t--){
-        cin>>n;
-        int sx,sy,dx,dy;
-        cin>>sx>>sy>>dx>>dy;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<5;j++){
-                cin>>point[i][j];
-            }
+    cin>>n;
+    cin>>sx>>sy>>dx>>dy;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<5;j++){
+            cin>>grid_worm[i][j];
         }
-        int ans=1e4;
-        short_dis_to(n,sx,sy,dx,dy,0,ans);
-        short_dis_by(n,dx,dy,sx,sy,0,ans);
-        cout<<ans<<endl;
     }
+    ans=1e6;
+    fn(sx,sy,dx,dy,0);
+    cout<<ans<<endl;
     return 0;
 }
