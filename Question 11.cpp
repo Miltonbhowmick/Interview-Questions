94d50335c9cb287e3c6b204291a584820d386951
Milton Chandro Bhowmick <miltonbhowmick6@gmail.com>
2020-02-07 20:22:23 +0600

    Add files via upload

--- Question 11.cpp
+++ Question 11.cpp
@@ -1,57 +1,59 @@
 #include<iostream>
+#define MAX 1000
 using namespace std;
-int graph[15][15],input[15][2];
-bool visited[15];
-int V,E,save;
+int grid[MAX][MAX],n,sx,sy,dx,dy,input[MAX][2],v;
+bool vis[MAX];
 
-int abs(int a){
-   if(a<0) return -1*a;
-   else return a;
+int abs(int x){
+    if(x<0) return -1*x;
+    return x;
 }
-void tsp(int src, int V, int cnt, int cost, int &ans){
+void tsp(int u, int cnt,int cost,int &ans){
 
-    if(cnt==V&&graph[src][0]){
-        ans = min(ans, cost+graph[src][V]);
+    if(cnt==n){
+        ans = min(ans, cost+grid[u][n+1]);
         return ;
     }
-    for(int i=0;i<V;i++){
-        if(!visited[i]&&graph[src][i]){
-            visited[i]=true;
-            tsp(i,V,cnt+1,cost+graph[src][i],ans);
-            visited[i]=false;
+
+    for(int i=1;i<=n;i++){
+        if(grid[u][i]){
+            if(vis[i]==false){
+                vis[i]=true;
+                tsp(i,cnt+1,cost+grid[u][i],ans);
+                vis[i]=false;
+            }
         }
     }
 }
 void reset(){
-    for(int i=0;i<=15;i++){
-        for(int j=0;j<=15;j++){
-            graph[i][j]=abs(input[i][0]-input[j][0])+abs(input[i][1]-input[j][1]);
+    for(int i=0;i<=n+1;i++){
+        for(int j=0;j<=n+1;j++){
+            grid[i][j]=abs(input[i][0]-input[j][0])+abs(input[i][1]-input[j][1]);
         }
-        visited[i]=false;
+        vis[i]=false;
     }
 }
 int main()
 {
-    for(int t=0;t<10;t++){
-        int ans=1e3;
-        int c; //customers
-        cin>>c;
-        int x,y,p,q;
+    cin>>n;
+    cin>>sx>>sy>>dx>>dy;
+    input[0][0]=sx;
+    input[0][1]=sy;
+    for(int i=1;i<=n;i++){
+        int x,y;
         cin>>x>>y;
-        input[0][0]=x,input[0][1]=y;
-        cin>>p>>q;
-        for(int i=1;i<=c;i++){
-            cin>>x>>y;
-            input[i][0]=x;
-            input[i][1]=y;
-        }
-        input[c+1][0]=p,input[c+1][1]=q;
-        reset();
-
-        visited[0]=true;
-        tsp(0,c+1,1,0,ans);
-        cout<<"#"<<t+1<<" "<<ans<<endl;
+        input[i][0]=x;
+        input[i][1]=y;
     }
+    input[n+1][0]=dx;
+    input[n+1][1]=dy;
+    v=n+1;
+    int ans=1e6;
+    reset();
+    vis[0]=true;
+    tsp(0,0,0,ans);
+
+    cout<<ans<<endl;
 
     return 0;
 }
