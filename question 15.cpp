c4900e4a923d2ee2f7ef42b3b1fd73b20b5c9e98
Milton Chandro Bhowmick <miltonbhowmick6@gmail.com>
2020-02-08 00:31:45 +0600

    Add files via upload

--- question 15.cpp
+++ question 15.cpp
@@ -1,52 +1,54 @@
 #include<iostream>
+#define MAX 1000
 using namespace std;
-int points[10000][2];
-int graph[10000][10000];
-bool visited[10000];
+int grid[MAX][MAX],n,input[MAX][2],sx,sy,dx,dy;
+bool vis[MAX];
 
 int abs(int x){
     if(x<0) return -1*x;
     return x;
 }
-void tsp_path(int n,int pos,int cnt,int cost, int &ans){
-    if(cnt==n&&graph[pos][0]){
-        ans = min(ans, cost+graph[pos][n]);
+void tsp(int u,int cnt,int cost,int &ans){
+    if(cnt==n){
+        ans=min(ans,cost+grid[u][n+1]);
         return;
     }
-    for(int i=0;i<n;i++){
-        if(visited[i]==false){
-            visited[i]=true;
-            tsp_path(n,i,cnt+1,cost+graph[pos][i],ans);
-            visited[i]=false;
+    for(int i=1;i<=n;i++){
+        if(grid[u][i]){
+            if(vis[i]==false){
+                vis[i]=true;
+                tsp(i,cnt+1,cost+grid[u][i],ans);
+                vis[i]=false;
+            }
         }
     }
 }
 void reset(){
-    for(int i=0;i<10000;i++){
-        for(int j=0;j<10000;j++){
-            graph[i][j]=abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
+    for(int i=0;i<=n+1;i++){
+        for(int j=0;j<=n+1;j++){
+            grid[i][j]=abs(input[i][0]-input[j][0])+abs(input[i][1]-input[j][1]);
         }
-        visited[i]=false;
     }
 }
 int main()
 {
-    int n;
     cin>>n;
-    int sx,sy,dx,dy;
     cin>>sx>>sy>>dx>>dy;
-    points[0][0]=sx;
-    points[0][1]=sy;
+    input[0][0]=sx;
+    input[0][1]=sy;
     for(int i=1;i<=n;i++){
-        cin>>points[i][0]>>points[i][1];
+        cin>>input[i][0];
+        cin>>input[i][1];
     }
-    points[n+1][0]=dx;
-    points[n+1][1]=dy;
+    input[n+1][0]=dx;
+    input[n+1][1]=dy;
+
     reset();
 
     int ans=1e5;
-    visited[0]=true;
-    tsp_path(n+1,0,1,0,ans);
+    vis[0]=true;
+    tsp(0,0,0,ans);
     cout<<ans<<endl;
+
     return 0;
 }
